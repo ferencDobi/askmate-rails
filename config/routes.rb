f97d7fcc5189f14e190a8f9835ca512cf97bc4f2
ferencDobi <ferencdbi@gmail.com>
2019-03-05 17:43:10 +0100

    add question routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/about' => 'home#about'
+
+  get '/questions' => 'home#temp'
+
+  post '/questions' => 'home#temp'
+
+  get '/questions/:id' => 'home#temp'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
