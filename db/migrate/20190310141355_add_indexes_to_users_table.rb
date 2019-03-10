71e29bf2c250d219025c9a5f2a1e47daf9165b14
ferencDobi <ferencdbi@gmail.com>
2019-03-10 15:22:16 +0100

    add indexes to users table

---
+++ db/migrate/20190310141355_add_indexes_to_users_table.rb
@@ -0,0 +1,8 @@
+class AddIndexesToUsersTable < ActiveRecord::Migration[5.2]
+  def change
+    change_table :users do |t|
+      t.index :username, unique: true
+      t.index :email, unique: true
+    end
+  end
+end
