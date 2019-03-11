4b09e2761587105102e9eb70a9c574b6acd95a88
ferencDobi <ferencdbi@gmail.com>
2019-03-11 16:25:18 +0100

    fix new question user association

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -68,6 +68,6 @@ class AnswersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def answer_params
-    params.require(:answer).permit(:message, :image, :upvotes, :belongs_to, :belongs_to)
+    params.require(:answer).permit(:message, :image, :upvotes, :user_id, :question_id)
   end
 end
