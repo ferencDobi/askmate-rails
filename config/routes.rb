453e8b7f2e36750250e576768a83f113230dbed2
ferencDobi <ferencdbi@gmail.com>
2019-03-12 14:18:26 +0100

    delete temporary routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,5 @@ Rails.application.routes.draw do
   resources :questions
 
   get '/about' => 'home#about'
-
-  get '/questions' => 'home#temp'
-
-  post '/questions' => 'home#temp'
-
-  get '/questions/:id' => 'home#temp'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
