080bf0569d8bf9d568818f9796ef4cb8b1c17a18
ferencDobi <ferencdbi@gmail.com>
2019-03-14 14:57:42 +0100

    create votes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :answers
   root 'home#index'
 
   get 'signup' => 'users#new'
@@ -11,7 +10,15 @@ Rails.application.routes.draw do
   post 'login' => 'session#create'
   delete 'logout' => 'session#destroy'
 
-  resources :questions
+  resources :questions do
+    post 'vote/up' => 'question_vote#up'
+    post 'vote/down' => 'question_vote#down'
+
+    resources :answers
+
+    post 'answers/:id/vote/up' => 'answer_vote#up', as: 'answer_vote_up'
+    post 'answers/:id/vote/down' => 'answer_vote#down', as: 'answer_vote_down'
+  end
 
   get '/about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
