fab590b3826124b48fe586988164fbf2bba76275
ferencDobi <ferencdbi@gmail.com>
2019-03-15 12:35:27 +0100

    fix vote column name

--- db/migrate/20190313143616_create_votes_tables.rb
+++ db/migrate/20190313143616_create_votes_tables.rb
@@ -3,14 +3,14 @@ class CreateVotesTables < ActiveRecord::Migration[5.2]
     create_table :question_votes do |t|
       t.belongs_to :user
       t.belongs_to :question
-      t.column :vote, :integer, default: 0
+      t.column :direction, :integer, default: 0
       t.index %i[user_id question_id], unique: true
     end
 
     create_table :answer_votes do |t|
       t.belongs_to :user
       t.belongs_to :answer
-      t.column :vote, :integer, default: 0
+      t.column :direction, :integer, default: 0
       t.index %i[user_id answer_id], unique: true
     end
   end
