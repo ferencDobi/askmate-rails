1d1ecc7e656b63b1fd57720365f7ef0289e5c98d
ferencDobi <ferencdbi@gmail.com>
2019-03-16 15:02:18 +0100

    add registration sad paths

--- spec/features/register_user_spec.rb
+++ spec/features/register_user_spec.rb
@@ -18,4 +18,37 @@ feature 'Register user' do
     expect(user.email).to eq 'capy@bara.test'
     expect(user.password_digest).to_not eq 'password'
   end
+
+  scenario 'cannot register when confirmation password does not match' do
+    visit signup_path
+
+    within '#new_user' do
+      fill_in 'Username:', with: 'Capybara'
+      fill_in 'Password:', with: 'password'
+      fill_in 'Confirm password:', with: 'not_matching'
+      fill_in 'E-mail address:', with: 'capy@bara.test'
+      click_button 'Register'
+    end
+
+    expect(page).to have_content 'Passwords must match.'
+    expect(User.last).to eq nil
+  end
+
+  scenario 'cannot register when username or email already taken' do
+    User.create(username: 'Capybara', email: 'capy@bara.test',
+                password_digest: BCrypt::Password.create('password'))
+
+    visit signup_path
+
+    within '#new_user' do
+      fill_in 'Username:', with: 'Capybara'
+      fill_in 'Password:', with: 'password'
+      fill_in 'Confirm password:', with: 'password'
+      fill_in 'E-mail address:', with: 'capy@bara.test'
+      click_button 'Register'
+    end
+
+    expect(page).to have_content 'Username or email already in use.'
+    expect(User.where(username: 'Capybara').count).to eq 1
+  end
 end
